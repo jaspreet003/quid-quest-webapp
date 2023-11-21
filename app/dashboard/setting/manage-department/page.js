b58d7c46b5f1536c1dc5587ad900d1ef063491b5
jaspreet <jkaur003@gmail.com>
2023-11-21 14:29:13 -0500

    first draft

---
+++ app/dashboard/setting/manage-department/page.js
@@ -0,0 +1,17 @@
+export default function manageDepartmentPage() {
+    return <div className="lg:w-1/3 lg:flex lg:flex-row lg:justify-center">
+        <div className="lg:flex lg:flex-row lg:items-center">
+            <ul role="list" class="divide-y divide-gray-100">
+                <li class="flex items-center justify-between gap-x-6 py-5">
+                    <div class="flex min-w-0 gap-x-4">
+                        <p class="text-sm font-semibold leading-6 text-gray-900">Leslie Alexander</p>
+                    </div>
+                    <a href="#" class="rounded-full bg-white px-2.5 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">View</a>
+                </li>
+
+            </ul>
+        </div>
+
+    </div>
+}
+
